e5e1018437e536ecabcda0db5b2a3c18e71b7e48
josegabtech <jose.gabriel.tech@gmail.com>
2026-03-17 16:08:18 -0300

    primeiro exercício

---
+++ queue-js-2026/Fila.js
@@ -0,0 +1,20 @@
+class Fila{
+
+    #inicio;
+    #fim;
+    #qtd;
+    #elementos;
+    
+    constructor(tamanhoVetor = 10){
+
+        this.#fim = -1;
+        this.#inicio = 0;
+        this.#qtd = 0;
+        this.#elementos = new Array(tamanhoVetor);
+    }
+
+    //enqueue
+    //isFull
+    //dequeue
+    //isEmpty
+}   

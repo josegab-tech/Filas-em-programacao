55b0a722f91a49fadc2d62af5e102fefdbae361e
josegab-tech <jose.gabriel.tech@gmail.com>
2026-03-25 19:26:46 -0300

    Organização visual

--- controller/filaController.js
+++ controller/filaController.js
@@ -3,18 +3,17 @@ const minhaFila = new Fila(5);
 function adicionarElemento() {
   const novoElemento = document.getElementById("txtnovoNome");
   if (minhaFila.enqueue(novoElemento.value)) {
-    mostrarFila(); 
-    novoElemento.value = "" // mostrar a fila
+    mostrarFila();
+    novoElemento.value = ""; // mostrar a fila
   } else {
     alert("Fila cheia!");
   }
-  
 }
 
-function mostrarFila(){
-    const filaElemento = document.getElementById("lblPessoasFila");
-    filaElemento.textContent = minhaFila.toString();
-  }
+function mostrarFila() {
+  const filaElemento = document.getElementById("lblPessoasFila");
+  filaElemento.textContent = minhaFila.toString();
+}
 
 function removerElemento() {
   const itemRemovido = minhaFila.dequeue();
@@ -25,5 +24,4 @@ function removerElemento() {
   } else {
     alert("A fila já está vazia!");
   }
-
 }
